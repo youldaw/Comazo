e371f1b81b3725d3176fca547e3094011fb6a418
youldaw <68648932+youldaw@users.noreply.github.com>
2022-08-29 05:00:12 +0500

    card modal
    card modal

--- js/script.js
+++ js/script.js
@@ -129,7 +129,54 @@ $(function (){
                 }
             });
         }
-    })
+    });
+
+
+    $('.modal-slide').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        center: false,
+        smartSpeed:900,
+        items:4,
+        navText: ['<img src="images/slide-left-arrow.svg">','<img src="images/slide-right-arrow.svg">'],
+        responsive:{
+            0:{
+                items:2,
+            },
+            768:{
+                items:2,
+            },
+            900:{
+                items:3,
+            },
+            1025:{
+                items:4,
+            }
+        }
+    });
+
+
+
+
+
+
+
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
 
 
 });
