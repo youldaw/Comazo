c3a7b27ca47c077e2dc664936249e71ff136d952
youldaw <68648932+youldaw@users.noreply.github.com>
2022-09-10 20:04:42 +0500

    new page
    new page termo

--- js/script.js
+++ js/script.js
@@ -348,6 +348,74 @@ $(function (){
     });
 
 
+    jQuery(document).ready(function() {
+        $('.upper').on('input', setFill);
+        $('.lower').on('input', setFill);
+
+        var max = $('.upper').attr('max');
+        var min = $('.lower').attr('min');
+
+        function setFill(evt) {
+            var valUpper = $('.upper').val();
+            var valLower = $('.lower').val();
+            if (parseFloat(valLower) > parseFloat(valUpper)) {
+                var trade = valLower;
+                valLower = valUpper;
+                valUpper = trade;
+            }
+            
+            var width = valUpper * 100 / max;
+            var left = valLower * 100 / max;
+            $('.fill').css('left', 'calc(' + left + '%)');
+            $('.fill').css('width', width - left + '%');
+            
+            // Update info
+            if (parseInt(valLower) == min) {
+                $('.easy-basket-lower').val('0 ₽');
+            } else {
+                $('.easy-basket-lower').val(parseInt(valLower) + ' ₽');
+            }
+            if (parseInt(valUpper) == max) {
+                $('.easy-basket-upper').val('5000 ₽');
+            } else {
+                $('.easy-basket-upper').val(parseInt(valUpper) + ' ₽');
+            }
+        }
+        
+        $('.easy-basket-filter-info input').keyup(function() {
+            var valUpper = $('.easy-basket-upper').val();
+            var valLower = $('.easy-basket-lower').val();
+            var width = valUpper * 100 / max;
+            var left = valLower * 100 / max;
+            if ( valUpper > 5000 ) {
+                var left = max;
+            }
+            if ( valLower < 0 ) {
+                var left = min;
+            } else if ( valLower > max ) {
+                var left = min;
+            }
+            $('.fill').css('left', 'calc(' + left + '%)');
+            $('.fill').css('width', width - left + '%');
+            // меняем положение ползунков
+            $('.lower').val(valLower);
+            $('.upper').val(valUpper);
+        });
+        $('.easy-basket-filter-info input').focus(function() {
+            $(this).val('₽');
+        });
+        $('.easy-basket-lower').blur(function() {
+            var valLower = $('.lower').val('');
+            $(this).val(Math.floor(valLower));
+        });
+        $('.easy-basket-upper').blur(function() {
+            var valUpper = $('.upper').val('');
+            $(this).val(Math.floor(valUpper));
+        });
+        
+    });
+
+
 });
 
 
