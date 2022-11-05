dc440ca98b885273554b0f3b6ee8a67077575fa1
youldaw <68648932+youldaw@users.noreply.github.com>
2022-11-06 01:18:22 +0500

    termo-page
    termo-page

--- js/script.js
+++ js/script.js
@@ -516,6 +516,12 @@ $(function (){
         return false;
     });
 
+    $('.navbar-nav .dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+
 
 
 
